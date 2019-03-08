b2d78a80197ec34f4eb283a64e5becb779827f61
Devordxgkg <48325202+Devordiscord@users.noreply.github.com>
2019-03-08 13:20:08 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,6 @@ client.on('message', message => {
   if(!message.channel.guild) return;
 if(message.content.startsWith('$bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
 let copy = "LegendryBot";
 let request = `Requested By ${message.author.username}`;
